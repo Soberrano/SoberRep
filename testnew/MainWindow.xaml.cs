5f1051171565474fe8839becf8fc04de078120c6
Игорь Кривенко <igor125k@mail.ru>
2018-01-04 17:33:34 +0300

    Парам

---
+++ testnew/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace testnew
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Student> ListOfStudent = new List<Student>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddButtonClick = new LambdaCommand((param) =>
+            {
+                return EmptyLi();
+            },
+                (param) =>
+                {
+                    AddToList();
+                });
+
+            ChangeButtonClick = new LambdaCommand((param) => { return true; }, (param) => { Reload(); });
+            DataContext = this;
+        }
+        bool EmptyLi()
+        {
+            if (!string.IsNullOrEmpty(TBfio) && !string.IsNullOrEmpty(TBgroup) && !string.IsNullOrEmpty(TBfacult) && !string.IsNullOrEmpty(TBspec) && !string.IsNullOrEmpty(TBcourse))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Добавление нового студента в ListView
+        /// </summary>
+        public void AddToList()
+        {
+            Student student = new Student { FIO = TBfio, Facult = TBfacult, Spec = TBspec, Course = TBcourse, Group = TBgroup };
+            ListViewItem lvi = new ListViewItem()
+            {
+                Content = student
+            };
+            ListOfStudent.Add(student);
+            theListView.Items.Add(lvi);
+            Serializationer.WriteXml(ListOfStudent);
+        }
+        public void Reload()
+        {
+
+        }
+
+       
+        #region Рабочие лошадки
+
+        string _tbfio;
+        public string TBfio
+        {
+            get
+            {
+                return _tbfio;
+            }
+            set
+            {
+                _tbfio = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("TBfio"));
+                AddButtonClick.CanExeсChanged();
+
+            }
+        }
+        string _tbgroup;
+        public string TBgroup
+        {
+            get
+            {
+                return _tbgroup;
+            }
+            set
+            {
+                _tbgroup = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("TBgroup"));
+
+                AddButtonClick.CanExeсChanged();
+
+            }
+        }
+        string _tbfacult;
+        public string TBfacult
+        {
+            get
+            {
+                return _tbfacult;
+            }
+            set
+            {
+                _tbfacult = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("TBfacult"));
+
+                AddButtonClick.CanExeсChanged();
+
+            }
+        }
+        string _tbspec;
+        public string TBspec
+        {
+            get
+            {
+                return _tbspec;
+            }
+            set
+            {
+                _tbspec = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("TBspec"));
+
+                AddButtonClick.CanExeсChanged();
+
+            }
+        }
+        string _tbcourse;
+        public string TBcourse
+        {
+            get
+            {
+                return _tbcourse;
+            }
+            set
+            {
+                _tbcourse = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("TBcourse"));
+
+                AddButtonClick.CanExeсChanged();
+
+            }
+        }
+
+
+
+        public LambdaCommand AddButtonClick { get; set; }
+        public LambdaCommand ChangeButtonClick { get; set; }
+
+        Serializationer ser = new Serializationer();
+
+        public class LambdaCommand : ICommand
+        {
+            delegate bool CanExecutedelegate(object nekoeOpisanieCanExec);
+            delegate void Executedelegate(object nekoeOpisanieExec);
+
+            CanExecutedelegate _CanExecuteDelegate;
+            Executedelegate _ExecuteDelegate;
+
+
+            public LambdaCommand(Func<object, bool> konkretnoeOpisanieCanExec, Action<object> konkretnoeOpisanieExec)
+            {
+
+                _CanExecuteDelegate = new CanExecutedelegate(konkretnoeOpisanieCanExec);
+                _ExecuteDelegate = new Executedelegate(konkretnoeOpisanieExec);
+            }
+
+
+            public bool CanExecute(object exemplarDelegata)
+            {
+                return _CanExecuteDelegate(exemplarDelegata);
+            }
+            public void Execute(object exemplarDelegata)
+            {
+                _ExecuteDelegate(exemplarDelegata);
+            }
+            public void CanExeсChanged()
+            {
+                if (CanExecuteChanged != null)
+                    CanExecuteChanged.Invoke(this, new EventArgs());
+            }
+            public event EventHandler CanExecuteChanged;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ser.GetStudents();
+        }
+    }
+    #endregion
+    [Serializable]
+    [XmlRoot("Student")]
+    public class Student
+    {
+        [XmlAttribute("ФиоСтудента")]
+        public string FIO { get; set; }
+        [XmlAttribute("Факультет")]
+        public string Facult { get; set; }
+        [XmlAttribute("Специальность")]
+        public string Spec { get; set; }
+        [XmlAttribute("Курс")]
+        public string Course { get; set; }
+        [XmlAttribute("Группа")]
+        public string Group { get; set; }
+    }
+
+
+}
---
+++ testnew/Serializationer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace testnew
+{
+    public class Serializationer : MainWindow
+    {
+        public static void WriteXml(List<Student> student)
+        {
+
+            using (StreamWriter writer = new StreamWriter("Serialization001.xml"))
+            {
+                try
+                {
+                    XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                    ns.Add("", "");
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+                    serializer.Serialize(writer, student, ns);
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка сохранения");
+                }
+                finally
+                {
+                    writer.Close();
+                }
+
+            }
+
+        }
+
+        public void GetStudents()
+        {
+            var xmlser = new XmlSerializer(typeof(Student));
+            string filename = "Serialization001.xml";
+            if (File.Exists(filename))
+            {
+                using (FileStream fs = new FileStream(filename, FileMode.Open))
+                {
+                    var newStudent = (Student)xmlser.Deserialize(fs);
+                    ListViewItem lvi = new ListViewItem()
+                    {
+                        Content = newStudent
+                    };
+                    ListOfStudent.Add(newStudent);
+                    theListView.Items.Add(lvi);
+
+                }
+            }
+            //Student student = null;
+            //string filename = "Serialization001.xml";
+            //XmlSerializer xser = new XmlSerializer(typeof(Student));
+            //if (File.Exists(filename))
+            //{
+            //    using (StreamReader fs = new StreamReader(filename))
+            //    {
+            //        student = (Student)xser.Deserialize(fs);
+            //        fs.Close();
+            //    }
+
+            //}
+        }
+    }
+}
